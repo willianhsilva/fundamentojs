01a07ad7b54c82d05ee75ba2edb82853962e395c
Willian Holanda <willianholandasilva@gmail.com>
2023-01-04 19:50:53 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,10 @@
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const text1 = 'Alura';
+const text2 = 'alura';
+
+console.log(text1 === text2)
---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = 'Olá, mundo!';
+const texto2 = "Olá, mundo!";
+const senha = 'senhaSuperSegura456!';
+const StringDeNumeros = '34567';
+
+const citacao = 'Meu nome é ';
+const meuNome = 'Willian'
+
+//concatenacao (+)
+
+console.log(citacao + meuNome)
+
+//tempate string ou template literal
+
