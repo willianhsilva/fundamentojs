1aa2d6b3db092eb16ee4ef1973933ed5a400c192
Willian Holanda <willianholandasilva@gmail.com>
2023-01-04 21:14:33 -0300

    codigos var e conversoes

---
+++ conversao.js
@@ -0,0 +1,14 @@
+//tipo de dado
+//booleanos
+
+//conversão implicita
+const numero = 456;
+const numeroString = "456";
+
+//Number()
+//String()
+
+console.log(numero + Number(numeroString))
+
+//conversão explicita
+
---
+++ script.js
@@ -0,0 +1,34 @@
+//var 
+
+/* var altura = 5;
+var cumprimento = 7;
+
+area = altura * cumprimento;
+console.log(area);
+var area; */
+
+/* let forma = 'retângulo';
+let altura = 5;
+let cumprimento = 7;
+let area;
+
+if(forma === 'retângulo'){
+    area = altura * cumprimento;
+}else{
+    area = (altura * cumprimento) / 2;
+}
+
+console.log(area) */
+
+const forma = 'triângulo';
+const altura = 5;
+const cumprimento = 7;
+let area;
+
+if(forma === 'quadrado'){
+    area = altura * cumprimento;
+}else{
+    area = (altura*cumprimento)/2;
+}
+
+console.log(area)
