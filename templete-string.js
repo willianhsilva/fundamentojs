53ea51b904144c73dce9ec0686fe77e041da4d81
Willian Holanda <willianholandasilva@gmail.com>
2023-01-05 19:47:15 -0300

    nova forma de usar string

---
+++ templete-string.js
@@ -0,0 +1,10 @@
+const nome = 'Willian'
+const idade = 2023-1991
+const cidadeDeNascimento = 'Maceió'
+
+// const apresentacao = 'meu nome é ' + nome + ', minha idade é ' + idade + ' e nasci na cidade de ' + cidadeDeNascimento
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`
+
+console.log(apresentacao)
+
